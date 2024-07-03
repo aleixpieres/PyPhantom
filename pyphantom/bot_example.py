e30d5976f47ec8989dc65fe9d01a95cc5060724a
Aleix Pieres <55714906+aleixpieres@users.noreply.github.com>
2024-07-03 07:43:44 +0000

    testing summary option

--- pyphantom/bot_example.py
+++ pyphantom/bot_example.py
@@ -22,3 +22,35 @@ async def on_message(message):
         await message.channel.send(response['response'])
 
 client.run('your-token')
+
+
+'''
+import discord
+import ollama
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if client.user.mentioned_in(message):
+
+        words = message.content.split()
+        pyphantom_propmt = ' '.join(words[1:])
+
+        response = ollama.generate(model='llama3', prompt=pyphantom_propmt + 'under 100 characters')
+        await message.channel.send(response['response'])
+
+client.run('your-token')
+
+
+'''
--- pyphantom/pyphantom.py
+++ pyphantom/pyphantom.py
@@ -12,46 +12,14 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if client.user.mentioned_in(message):
-
-        words = message.content.split()
-        pyphantom_propmt = ' '.join(words[1:])
-
-        response = ollama.generate(model='llama3', prompt=pyphantom_propmt + 'under 100 characters')
-        await message.channel.send(response['response'])
-
-client.run('your-token')
-
-'''
-import discord
-import ollama
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if client.user.mentioned_in(message):
-
-        words = message.content.split()
-        pyphantom_propmt = ' '.join(words[1:])
-
-        response = ollama.generate(model='llama3', prompt=pyphantom_propmt + 'under 100 characters')
-        await message.channel.send(response['response'])
-
-client.run('your-token')
+    words = message.content.split()
+    message = ' '.join(words[1:])
+    if message.content == str('summary'):
+        c_channel = discord.utils.get(message.guild.text_channels, name='pyphantom')
+        messages = [message async for message in c_channel.history(limit=5)]
+   
+        await message.channel.send(messages.len())
+  
+client.run('')
 
 
-'''
